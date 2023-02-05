ca2632a33c46f1f0909aad51f500ad8ea56cedcf
russellsteadman <russell@steadman.email>
2023-02-05 15:36:30 +0000

    Deploying to gh-pages from @ russellsteadman/cmap@0f70ac5db951b8ef1dda83b33086d84084b20850 🚀

--- _next/static/3iF3TH0C_mICC9d7AA21A/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bbeadf4e6460e4a2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
